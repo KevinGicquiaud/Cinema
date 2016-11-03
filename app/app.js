b7e9af2968a3b5637a17bf53bdd7a05879f7f507
Alexandre Bertrand <alexandrebertrand96@gmail.com>
2016-11-03 15:27:08 +0100

    Affichage de la map Affichage des cinemas sur la map

--- app/app.js
+++ app/app.js
@@ -2,23 +2,22 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', ['ui.router', 'ngMap', 'myApp.detailsCinema', 'myApp.listeCinema'])
-    .config(['$stateProvider','uiGmapGoogleMapApiProvider', router]);
+    .config(['$stateProvider', router]);
 
 function router($stateProvider) {
-  var listeCinemaState = {
-    name: 'listeCinema',
-    url: '/',
-    templateUrl: 'views/listeCinema.html',
-    controller: 'listeCinemaCtrl'
-  };
-  var detailsCinemaState = {
-    name: 'detailsCinema',
-    url: '/details_cinema',
-    templateUrl: 'views/detailsCinema.html',
-    controller: 'DetailsCinemaCtrl'
-  };
+    var listeCinemaState = {
+        name: 'listeCinema',
+        url: '/',
+        templateUrl: 'views/listeCinema.html',
+        controller: 'listeCinemaCtrl'
+    };
+    var detailsCinemaState = {
+        name: 'detailsCinema',
+        url: '/details_cinema',
+        templateUrl: 'views/detailsCinema.html',
+        controller: 'DetailsCinemaCtrl'
+    };
 
-  $stateProvider.state(listeCinemaState);
-  $stateProvider.state(detailsCinemaState);
+    $stateProvider.state(listeCinemaState);
+    $stateProvider.state(detailsCinemaState);
 }
-
--- app/js/controller/listeCinema.js
+++ app/js/controller/listeCinema.js
@@ -2,21 +2,13 @@
 
 angular.module('myApp.listeCinema', [])
 
-.controller("listeCinemaCtrl", ['$scope', '$http', function ($scope, $http, NgMap) {
+    .controller("listeCinemaCtrl", ['$scope', '$http', function ($scope, $http, NgMap) {
 
-  $http.get('data/cinema.json').then(function (response) {
-    $scope.cinemas = response.data;
-  });
+        $http.get('data/cinema.json').then(function (response) {
+            $scope.cinemas = response.data;
+        });
 
-  var vm = this;
-  NgMap.getMap().then(function(map) {
-    vm.showCustomMarker= function(evt) {
-      map.customMarkers.foo.setVisible(true);
-      map.customMarkers.foo.setPosition(this.getPosition());
-    };
-    vm.closeCustomMarker= function(evt) {
-      this.style.display = 'none';
-    };
-  });
+        var vm = this;
+        $scope.map = {center: {latitude: 46.1577, longitude: -1.15359}, zoom: 8};
 
-}]);
+    }]);
